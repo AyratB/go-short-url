d0df8d412196c49e0382da8530f91508d4a0cb21
Ayrat Burnashev <airatbyr@ya.ru>
2022-04-21 10:28:33 +0300

    feat: add handlers

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,65 +1,17 @@
 package main
 
 import (
-	shortener "github.com/AyratB/go-short-url/internal/app"
+	"github.com/AyratB/go-short-url/internal/handlers"
 	"github.com/gorilla/mux"
-	"io"
 	"log"
 	"net/http"
 )
 
-func GetHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Only GET requests are allowed by this route!", http.StatusMethodNotAllowed)
-		return
-	}
-
-	vars := mux.Vars(r)
-	id := vars["id"]
-
-	if len(id) == 0 {
-		http.Error(w, "Need to set id", http.StatusMethodNotAllowed)
-		return
-	}
-
-	longURL, err := shortener.GetRawURL(id)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-	}
-	w.Header().Set("Location", longURL)
-	w.WriteHeader(http.StatusTemporaryRedirect)
-}
-
-func PostHandler(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method != http.MethodPost {
-		http.Error(w, "Only POST requests are allowed by this route!", http.StatusMethodNotAllowed)
-		return
-	}
-
-	rawURL, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
-	shortURL, err := shortener.MakeShortURL(string(rawURL))
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(shortURL))
-}
-
 func main() {
 
 	router := mux.NewRouter()
-	router.HandleFunc("/{id}", GetHandler)
-	router.HandleFunc("/", PostHandler)
+	router.HandleFunc("/{id}", handlers.GetURLHandler)
+	router.HandleFunc("/", handlers.SaveURLHandler)
 
 	http.Handle("/", router)
 
